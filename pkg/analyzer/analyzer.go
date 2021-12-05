b2cb7b7c8051fe21023cfc76fc7c0dc72aa5d1ea
Serg_Kotovsky <rookulab@mail.ru>
2021-12-05 22:38:12 +0300

    try to build own linter

--- pkg/analyzer/analyzer.go
+++ pkg/analyzer/analyzer.go
@@ -1,7 +1,36 @@
 package analyzer
 
-import "golang.org/x/tools/go/analysis"
+import (
+	"flag"
+	"go/ast"
+
+	"golang.org/x/tools/go/analysis"
+)
+
+//nolint:gochecknoglobals
+var flagSet flag.FlagSet
 
 func NewAnalyzer() *analysis.Analyzer {
-	return nil
+	return &analysis.Analyzer{
+		Name:  "mylinter",
+		Doc:   "finds bad words about teamlead",
+		Run:   run,
+		Flags: flagSet,
+	}
+}
+
+func run(pass *analysis.Pass) (interface{}, error) {
+	for _, f := range pass.Files {
+		// parse file
+		ast.Inspect(f, func(node ast.Node) bool {
+			f, ok := node.(*ast.Comment)
+			if !ok {
+				return true
+			}
+			pass.Reportf(node.Pos(), "calculated cyclomatic complexity for function %s is %d, max is %d", f.Name.Name, comp, maxComplexity)
+
+		})
+	}
+
+	return nil, nil
 }
